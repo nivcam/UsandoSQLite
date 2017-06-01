e21de6e50fe060adae1d416590a5b772e7fd279d
Nicolas <nivcam@gmail.com>
2017-06-01 12:08:35 -0300

    04

--- app/src/main/java/com/example/nicolas/usandosqlite/ListaActivity.java
+++ app/src/main/java/com/example/nicolas/usandosqlite/ListaActivity.java
@@ -2,12 +2,24 @@ package com.example.nicolas.usandosqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 public class ListaActivity extends AppCompatActivity {
+
+    private ListView lvregistros;
+    private SQLiteDatabase banco;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
+
+        lvregistros = (ListView) findViewById(R.id.lvlistar);
+
+        banco = this.openOrCreateDatabase("banco", getApplicationContext().MODE_PRIVATE, null);
+        banco.execSQL("CREATE TABLE IF NOT EXISTS notas (_id INTEGER PRIMARY KEY AUTOINCREMENT, " + "nome_disciplina TEXT NOT NULL, nota DECIMAL NOT NULL");
+
     }
 }
--- app/src/main/java/com/example/nicolas/usandosqlite/MainActivity.java
+++ app/src/main/java/com/example/nicolas/usandosqlite/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.nicolas.usandosqlite;
 
+import android.content.ContentValues;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import android.database.sqlite.SQLiteDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText etcodigo, etnome_disciplina, etnota;
     private Button btincluir, btalterar, btexcluir, btpesquisar, btlistar;
+    private SQLiteDatabase banco;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,29 @@ public class MainActivity extends AppCompatActivity {
         btexcluir = (Button) findViewById(R.id.btexcluir);
         btpesquisar = (Button) findViewById(R.id.btpesquisar);
         btlistar = (Button) findViewById(R.id.btlistar);
+    }
+    public void btincluirOnClick(View v) {
+        ContentValues registro = new ContentValues();
+        registro.put("nome_disciplina", etnome_disciplina.getText().toString());
+        registro.put("nota", Double.parseDouble(etnota.getText().toString()));
+        banco.insert("notas", null, registro);
+        Toast.makeText(getApplicationContext(), "Sucesso!", Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void btalterarOnClick(View v) {
+
+    }
+
+    public void btexcluirOnClick(View v) {
+
+    }
+
+    public void btpesquisarOnClick(View v) {
+
+    }
+
+    public void btlistarOnClick(View v) {
 
     }
 }
